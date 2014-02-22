2d20ef30bff39c5e3f9a580cafd7a0bc3dde4b8e
Aruna <arunzzrip@gmail.com>
2014-02-22 10:30:43 +0530

    add new form

---
+++ Billing System/billing system/billing system/Classes/ReorderForm.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MySql.Data.MySqlClient;
+
+
+namespace billing_system.Classes
+{
+    class ReorderForm
+    {
+
+        public void reorderLevel(object obj)
+        {
+            DBConnection db = new DBConnection();
+            int billno = 0; 
+            try
+            {
+                string query = "SELECT COUNT(Item_Code) FROM alert";
+                int count = 0; //initialize count variable
+
+
+
+                if (db.OpenConnection() == true)
+                {
+                    MySqlCommand cmd = new MySqlCommand(query, db.connection);
+                    count = int.Parse(cmd.ExecuteScalar().ToString()); //check for existing bills
+
+
+
+                    if (count < 1)
+                        billno = 000001;    //first billno
+                }
+            }
+            catch (Exception e)
+            {
+
+
+            }
+
+        }
+    }
+}
