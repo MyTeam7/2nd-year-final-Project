e082a5d4450b0c86f4efa4717c66e1c4b8292717
dilpro <dilankadevelopment@gmail.com>
2014-02-23 15:00:27 +0530

    Error Free Version

--- Billing System/billing system/billing system/Form2.cs
+++ Billing System/billing system/billing system/Form2.cs
@@ -574,7 +574,7 @@ namespace billing_system
             }
         }
 
-<<<<<<< HEAD
+
         private void Qty_Click(object sender, EventArgs e)
         {
 
@@ -583,12 +583,12 @@ namespace billing_system
         private void panel6_Paint(object sender, PaintEventArgs e)
         {
 
-=======
+        }
         private void Reorder_Click(object sender, EventArgs e)
         {
             Form5 reorder = new Form5();
             reorder.Show();
->>>>>>> origin/Aruna
+
         }
 
     }
