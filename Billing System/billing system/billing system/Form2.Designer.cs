162a8e6e12640d990361e60a9e31f84f83d3496e
dilpro <dilankadevelopment@gmail.com>
2014-02-25 20:14:50 +0530

    without head

--- Billing System/billing system/billing system/Form2.Designer.cs
+++ Billing System/billing system/billing system/Form2.Designer.cs
@@ -455,11 +455,11 @@
             this.txtBoxCode.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.txtBoxCode.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtBoxCode.Location = new System.Drawing.Point(51, 15);
-<<<<<<< HEAD
+
             this.txtBoxCode.MaxLength = 10;
-=======
+
             this.txtBoxCode.MaxLength = 5;
->>>>>>> origin/Dilanka
+
             this.txtBoxCode.Name = "txtBoxCode";
             this.txtBoxCode.Size = new System.Drawing.Size(186, 26);
             this.txtBoxCode.TabIndex = 1;
